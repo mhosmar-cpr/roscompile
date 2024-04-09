2bc568ee65d213f1485981600f08b34d45913bfa
Michael Hosmar <mikehosmar@gmail.com>
2024-04-09 19:56:12 -0400

    class node style

--- magical_ros2_conversion_tool/src/magical_ros2_conversion_tool/pythonic.py
+++ magical_ros2_conversion_tool/src/magical_ros2_conversion_tool/pythonic.py
@@ -1,8 +1,9 @@
 PYTHON_REPLACEMENTS = {
-    r'\n(\s+)rospy.init_node\((.*)\)': '\n$0rclpy.init()\n$0node = rclpy.create_node($1)',
-    r'rospy.Publisher\(([^\,]+), ([^\)]+)\)': 'node.create_publisher($1, $0)',
-    r'rospy.Subscriber\(([^\,]+), ([^\,]+), ([^\)]+)\)': 'node.create_subscription($1, $0, $2)',
-    r'rospy.Service\(([^\,]+), ([^\,]+), ([^\)]+)\)': 'node.create_service($1, $0, $2)',
+    r'\n(\s+)rospy.init_node\((.*)\)': '\n$0rclpy.init(args=args)\n$0node = ClassName($1)\n$0rclpy.spin(node)\n$0rclpy.shutdown()',
+    r'rospy.Publisher\(([^\,]+), ([^\)]+)\)': 'self.create_publisher($1, $0)',
+    r'rospy.Subscriber\(([^\,]+), ([^\,]+), ([^\)]+)\)': 'self.create_subscription($1, $0, $2)',
+    r'rospy.Service\(([^\,]+), ([^\,]+), ([^\)]+)\)': 'self.create_service($1, $0, $2)',
+    r'\n(\s+)rospy.get_parameter\(([^\,]+), ([^\)]+)\)': 'self.declare_parameter($1, $2)\n$0self.$1 = self.get_parameter($1).value',
 }
 
 
