5b053c165c0b6f1650838baba7c0aa27a4ec61c2
Michael Hosmar <mikehosmar@gmail.com>
2024-04-09 22:16:27 -0400

    small fixes,  add rcl time functions

--- magical_ros2_conversion_tool/src/magical_ros2_conversion_tool/pythonic.py
+++ magical_ros2_conversion_tool/src/magical_ros2_conversion_tool/pythonic.py
@@ -1,15 +1,19 @@
 PYTHON_REPLACEMENTS = {
-    r'\n(\s+)rospy.init_node\((.*)\)': '\n$0rclpy.init(args=args)\n$0node = ClassName($1)\n$0rclpy.spin(node)\n$0rclpy.shutdown()',
+    r'\n(\s+)rospy.init_node\((.*)\)': '\n$0rclpy.init(args=args)\n$0node = ClassName($1)\n$0rclpy.spin(node)\n$0node.destroy_node()\n$0rclpy.shutdown()',
     r'rospy.Publisher\(([^\,]+), ([^\)]+)\)': 'self.create_publisher($1, $0)',
     r'rospy.Subscriber\(([^\,]+), ([^\,]+), ([^\)]+)\)': 'self.create_subscription($1, $0, $2)',
     r'rospy.Service\(([^\,]+), ([^\,]+), ([^\)]+)\)': 'self.create_service($1, $0, $2)',
     r'\n(\s+)([^\s]+)[\ \t]*=[\ \t]*rospy\.get_param\(([^\,\n]+),[\ \t]*([^\s]+)\)': '\n$0$1 = self.declare_parameter($2, $3).value',
-    r'\n(\s+)([^\s]+)[\ \t]*=[\ \t]*rospy\.get_param\(([^\,\n]+)\)': '\n$0$1 = self.declare_parameter($2, rclpy.Parameter.Type.DOUBLE).value',
-    r'\n(\s+)rospy\.logdebug\(([^\,\n]+)\)': '\n$0self.get_logger().debug($1)',
-    r'\n(\s+)rospy\.loginfo\(([^\,\n]+)\)': '\n$0self.get_logger().info($1)',
-    r'\n(\s+)rospy\.logwarn\(([^\,\n]+)\)': '\n$0self.get_logger().warning($1)',
-    r'\n(\s+)rospy\.logerr\(([^\,\n]+)\)': '\n$0self.get_logger().error($1)',
-    r'\n(\s+)rospy\.logfatal\(([^\,\n]+)\)': '\n$0self.get_logger().fatal($1)',
+    r'\n(\s+)([^\s]+)[\ \t]*=[\ \t]*rospy\.get_param\(([^\s]+)\)': '\n$0$1 = self.declare_parameter($2, rclpy.Parameter.Type.DOUBLE).value',
+    r'\n(\s+)rospy\.logdebug\(([^\s]+)\)': '\n$0self.get_logger().debug($1)',
+    r'\n(\s+)rospy\.loginfo\(([^\s]+)\)': '\n$0self.get_logger().info($1)',
+    r'\n(\s+)rospy\.logwarn\(([^\s]+)\)': '\n$0self.get_logger().warning($1)',
+    r'\n(\s+)rospy\.logerr\(([^\s]+)\)': '\n$0self.get_logger().error($1)',
+    r'\n(\s+)rospy\.logfatal\(([^\s]+)\)': '\n$0self.get_logger().fatal($1)',
+    r'\n(\s+)rospy\.Duration\(([^\s]+)\)': '\n$0rclpy.duration.Duration($1)',
+    r'\n(\s+)rospy\.Time\.now\(\)': '\n$0self.get_clock().now()',
+    r'\n(\s+)rospy\.is_shutdown\(\)': '\n$0rclpy.ok()',
+    r'\n(\s+)rospy\.Rate\(([^\s]+)\)': '\n$0self.create_rate($1)',
 }
 
 
