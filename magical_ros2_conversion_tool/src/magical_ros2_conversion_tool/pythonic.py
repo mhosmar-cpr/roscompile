20b1ff06e4a5bc97cb1a59cbd309f0e6ebfaab68
Michael Hosmar <mikehosmar@gmail.com>
2024-04-09 21:21:53 -0400

    add logging

--- magical_ros2_conversion_tool/src/magical_ros2_conversion_tool/pythonic.py
+++ magical_ros2_conversion_tool/src/magical_ros2_conversion_tool/pythonic.py
@@ -5,6 +5,11 @@ PYTHON_REPLACEMENTS = {
     r'rospy.Service\(([^\,]+), ([^\,]+), ([^\)]+)\)': 'self.create_service($1, $0, $2)',
     r'\n(\s+)([^\s]+)[\ \t]*=[\ \t]*rospy\.get_param\(([^\,\n]+),[\ \t]*([^\s]+)\)': '\n$0$1 = self.declare_parameter($2, $3).value',
     r'\n(\s+)([^\s]+)[\ \t]*=[\ \t]*rospy\.get_param\(([^\,\n]+)\)': '\n$0$1 = self.declare_parameter($2, rclpy.Parameter.Type.DOUBLE).value',
+    r'\n(\s+)rospy\.logdebug\(([^\,\n]+)\)': '\n$0self.get_logger().debug($1)',
+    r'\n(\s+)rospy\.loginfo\(([^\,\n]+)\)': '\n$0self.get_logger().info($1)',
+    r'\n(\s+)rospy\.logwarn\(([^\,\n]+)\)': '\n$0self.get_logger().warning($1)',
+    r'\n(\s+)rospy\.logerr\(([^\,\n]+)\)': '\n$0self.get_logger().error($1)',
+    r'\n(\s+)rospy\.logfatal\(([^\,\n]+)\)': '\n$0self.get_logger().fatal($1)',
 }
 
 
